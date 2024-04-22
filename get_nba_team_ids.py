65301f416e8b74119cf0bcaab7952134e8893791
Rob Cary <163696652+curryblobcurry@users.noreply.github.com>
2024-04-22 08:48:00 -0400

    Add files via upload

---
+++ get_nba_team_ids.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 17:45:04 2024
+
+@author: robcary
+"""
+
+
+def get_nba_team_player_ids(team):
+#from return_player_id import return_p_id
+    import requests
+
+    url = 'http://api.balldontlie.io/v1/players'
+
+    headers = {
+        'Authorization':'53f7fae1-47e4-4649-8714-ae58eab679bf'
+        }
+
+    response = requests.get(url, headers=headers)
+
+    response_data = response.json()
+
+    #team = 'Milwaukee Bucks'
+    #team = input('Please enter an NBA Team name: ')
+
+    i = 0
+    cursor = 0
+    team_roster = []
+    while i<25:
+        if i!=24:
+            if response_data["data"][i]["team"]["full_name"]==team:
+                if len(team_roster)>2:
+                    if (response_data["data"][i]["id"]==team_roster[-1]):
+                        break
+                    team_roster.append(str(response_data["data"][i]["id"]))
+                    #print(f'{team_roster} ')
+                else:
+                    team_roster.append(str(response_data["data"][i]["id"]))
+                    #print(f'{team_roster} ')
+            i += 1
+        elif i==24:
+            if response_data["data"][i]["team"]["full_name"]==team:
+                if len(team_roster)>2:
+                    if (response_data["data"][i]["id"]==team_roster[-1]):
+                        break
+                else:
+                    team_roster.append(str(response_data["data"][i]["id"]))
+                    #print(f'{team_roster} ')
+        #else:
+            cursor += 25
+            if cursor==750:
+                break
+            response = requests.get(url, {"cursor":cursor}, headers=headers)
+            response_data = response.json()
+            i = 0
+    return team_roster
+
+get_nba_team_player_ids('Milwaukee Bucks')
